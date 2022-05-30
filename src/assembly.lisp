9f4078486742096f3215c8ad164e10af9fd6c71f
Vitaly Drogan <vitaliy.drogan@gmail.com>
2022-05-30 09:40:34 +0300

    try class based instructions

--- src/assembly.lisp
+++ src/assembly.lisp
@@ -9,8 +9,16 @@
 			   1)))
     (make-instance 'push-evm :n bytes-needed :data int)))
 
+(defun string-to-push (string)
+  (let ((string (if (and (>= (length string) 2)
+			 (string= "0x" (subseq string 0 2)))
+		    (subseq string 2)
+		    string)))
+    (integer-to-push (bitsmash:hex->int string))))
+
 (defun asm-raw (ins)
   (cond ((typep ins 'integer) (list (integer-to-push ins)))
+	((stringp ins) (list (string-to-push ins)))
 	((and (symbolp ins)
 	      (gethash ins *atom-mnemonics*))
 	 (list (funcall (gethash ins *atom-mnemonics*))))
@@ -34,36 +42,34 @@
 		   (t `(asm-raw (list ,i))))))))
 
 (defun resolve-jumplabels (asm)
-  (error "incorrect impl")
   (let ((label-positions (make-hash-table)))
-    (loop for ins in asm
-	  sum (cond ((typep ins 'push-label-evm) +jump-dest-size-bytes+)
-		    ((typep ins 'label-evm) 1)
-		    (t (length (to-bytecode ins))))
-	  into current-offset
-
-	  do (when (typep ins 'label-evm)
-	       (when (gethash (get-sym ins) label-positions)
-		 (error "Jump label occurs multiple times, label: ~a" (get-sym ins)))
-	       (setf (gethash (get-sym ins) label-positions) current-offset)))
+    (let ((offset 0))
+      (dolist (ins asm)
+	(when (typep ins 'label-evm)
+	  (assert (null (gethash (get-sym ins) label-positions)) ()
+		  "Jump label ~a occurs multiple times: ~a" (get-sym ins))
+	  (setf (gethash (get-sym ins) label-positions) offset))
+	(let ((ins-length (cond ((typep ins 'push-label-evm) (1+ *jump-dest-size-bytes*))
+				((typep ins 'label-evm) 1)
+				(t (length (to-bytecode ins))))))
+	  (setf offset (+ offset ins-length)))))
     (loop for ins in asm
 	  collect (cond ((typep ins 'push-label-evm)
 			 (let ((label-pos (gethash (get-sym ins) label-positions)))
 			   (unless label-pos
 			     (error "Jumplabel can't be resolved ~a" (get-sym ins)))
-			   (first (asm (push +jump-dest-size-bytes+ label-pos)))))
+			   (first (asm (push *jump-dest-size-bytes* label-pos)))))
 			((typep ins 'label-evm)
 			 (first (asm jumpdest)))
 			(t ins)))))
 
 (defun emit-bytecode (asm)
-  (apply #'concatenate '(vector (unsigned-byte 8))
-	 (loop for i in asm
-	       collect (let ((bytecode (to-bytecode i)))
-			 (unless bytecode
-			   (error "Failed to emit bytecode for instruction ~a" i))
-			 bytecode))))
-
-(defun emit-hex (asm)
-  (bitsmash:hex<- (emit-bytecode asm)))
+  (let ((asm (resolve-jumplabels asm)))
+    (bitsmash:hex<-
+     (apply #'concatenate '(vector (unsigned-byte 8))
+	    (loop for i in asm
+		  collect (let ((bytecode (to-bytecode i)))
+			    (unless bytecode
+			      (error "Failed to emit bytecode for instruction ~a" i))
+			    bytecode))))))
 
--- src/cl-evasm.lisp
+++ src/cl-evasm.lisp
@@ -3,4 +3,23 @@
 (in-package #:cl-evasm)
 
 
-(asm 2 3 (- +max-uint+ 1))
+(asm
+ 0 calldataload) 
+
+(asm 0 1 add)
+
+(defun hevm-exec (code)
+  (uiop:run-program (list "hevm" "exec" "--code" code "--gas" "10000000" "--jsontrace") :output '(:string :stripped t)))
+
+'(hevm-exec
+ (emit-bytecode
+  (asm (push-label 'haha) jump 1 2 3 (label 'haha) 4
+       0 mstore 32 0 return))) 
+
+
+(emit-bytecode
+ (asm "0xffee" calldataload 42 eq
+      (push-label 'haha) jumpi 1 2 3 (label 'haha) 4
+       0 mstore 32 0 return)) 
+
+;;; erc20 token
